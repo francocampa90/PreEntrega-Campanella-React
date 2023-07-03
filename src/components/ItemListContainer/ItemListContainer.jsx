4359d37e3b927549f5238755231ab889af941e20
Franco Campanella <francocampa90@gmail.com>
2023-07-03 14:40:10 -0300

    segundo commit

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -5,7 +5,6 @@ const ItemListContainer = ({servicios}) => {
     return(
         <div className='servicios_container'>
             <h2>SERVICIOS ELECTRICOS Y DE ILUMINACION</h2>
-            <hr />
             <p>{servicios}</p>
         </div>
         
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,29 +2,27 @@
 import shoppingCart from '../../assets/shopping-cart_115264.svg'
 
 
-
-export const NavBar = () => { 
+export const NavBar = () => {
 
     return (
-        <NavBar className="NavBar">
-            <div className="NavBar_container">
-                <h1 className='NavBar_logo'>VOLTIO</h1>
+        <header className="header">
+            <div className="header_container">
+                <h1 className='header_logo'>VOLTIO</h1>
                 <img src="" alt="" />
                 
                 <nav className='navbar'>
                     <a className="navbar_link" href="#">Inicio</a>
                     <a className="navbar_link" href="#">Servicios</a>
-                    <a className="navbar_link" href="#">Empresa</a>
-                    <a className="navbar_link" href="#">Proyectos</a>
+                    <a className="navbar_link" href="#">Productos</a>
                     <a className="navbar_link" href="#">Contacto</a>
                 </nav>  
-                <div className='div-cart'>
-                    <img className='cart' src={shoppingCart} alt="" />
-                    <p className='numerito'>1</p>
+                <div className='div_cart'>
+                     <img className='cart' src={shoppingCart} alt="" />
+                     <p className='numerito'>1</p>
                 </div>
  
              </div>
-        </NavBar>
+        </header>
     )
 }
 
