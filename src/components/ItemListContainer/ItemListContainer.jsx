1044f2f33ea15936a76843d8bedcd4bdab5ce95b
Franco Campanella <francocampa90@gmail.com>
2023-07-16 12:46:44 -0300

    helpers, data, ItemList e ItemCard

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,14 +1,60 @@
+import { useEffect, useState } from 'react'
 //import './ItemListContainer.scss'
+import { pedirDatos } from '../../helpers/pedirDatos'
+import ItemList from '../ItemList/ItemList'
 
 
-const ItemListContainer = ({servicios}) => {
-    return(
-        <div className='servicios_container'>
-            <h2>HOGAR</h2>
-            <p>{servicios}</p>
-        </div>
-        
+
+const ItemListContainer = () => {
+
+    const [productos, setProductos] = useState ([])
+
+    useEffect(() => {
+        pedirDatos ()
+        .then((res) => {
+                setProductos(res)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    }, [])
+    
+
+    return (
+        <div>
+            <ItemList productos = {productos}/>
+        </div>      
     )
 }
 
 export default ItemListContainer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const ItemListContainer = ({servicios}) => {
+//     return(
+//         <div className='servicios_container'>
+//             <h2>HOGAR</h2>
+//             <p>{servicios}</p>
+//         </div>
+        
+//     )
+// }
+
+// export default ItemListContainer
